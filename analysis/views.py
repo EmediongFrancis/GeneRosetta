7b5a99ee1cecd0462790bd12b8588fae35fc08bb
Emediong Francis <emediongfrancis@gmail.com>
2025-12-12 22:01:23 +0100

    feat: implement AnalyzeView for processing DNA sequences with support for FASTA and VCF file types

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from .serializers import AnalysisInputSerializer
+from .services.ingest import IngestService
+from .models import AnalysisProject
 
-# Create your views here.
+class AnalyzeView(APIView):
+    """
+    POST /api/analyze/
+    Accepts FASTA/VCF file OR raw text.
+    Returns: UUID of the created project.
+    """
+    def post(self, request):
+        serializer = AnalysisInputSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            try:
+                # 1. Call the Service Layer to get clean data
+                clean_sequence = IngestService.process(serializer.validated_data)
+                
+                # 2. Determine input type for DB record
+                input_type = 'TEXT'
+                if 'sequence_file' in serializer.validated_data: # type: ignore
+                    fname = serializer.validated_data['sequence_file'].name.lower() # type: ignore
+                    if fname.endswith('.fasta'): input_type = 'FASTA'
+                    elif fname.endswith('.vcf'): input_type = 'VCF'
+
+                # 3. Create the Database Record
+                # We handle the User if they are logged in, otherwise None (Guest)
+                user = request.user if request.user.is_authenticated else None
+                
+                project = AnalysisProject.objects.create(
+                    user=user,
+                    input_type=input_type,
+                    input_sequence=clean_sequence,
+                    status='PENDING'
+                )
+
+                # 4. Return the UUID "Receipt"
+                return Response({
+                    "id": project.id,
+                    "status": project.status,
+                    "message": "File accepted. Processing pending."
+                }, status=status.HTTP_201_CREATED)
+
+            except ValidationError as e:
+                # Catch errors from the Service layer (e.g. Invalid DNA chars)
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
