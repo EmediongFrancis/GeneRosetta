9ec5f1f3ca625c0a8fbc097f6ffa1efd26778de0
Emediong Francis <emediongfrancis@gmail.com>
2025-12-13 22:52:37 +0100

    feat: add URL pattern for ProjectStatusView to track analysis progress

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import AnalyzeView
+from .views import ProjectStatusView
 
 urlpatterns = [
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
+    path('status/<uuid:project_id>/', ProjectStatusView.as_view(), name='status'),
 ]
