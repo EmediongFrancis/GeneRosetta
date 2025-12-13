eb96e3ec700d0a7550523b3c8ed41194c36f71f6
Emediong Francis <emediongfrancis@gmail.com>
2025-12-13 21:37:14 +0100

    feat: add Celery configuration and settings for task management

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# CELERY SETTINGS
+# ------------------------------------------------------------------------------
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+# Optimization: If the worker dies, don't acknowledge the task as "done" until it actually finishes.
+CELERY_TASK_ACKS_LATE = True
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
